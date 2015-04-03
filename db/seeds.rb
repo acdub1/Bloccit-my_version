17169272448864305360168864d7b4b71cd41fdd
acdub1 <amberdub1nc@gmail.com>
2015-04-03 11:32:58 +0000

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
 require 'faker'
+
+#Create unique post
+post = Post.first
+unless Post.where(:title == 'Unique Post').exists?         
+      Post.create!(                 
+        title: 'Unique Post',
+        body:  'This is the body of a unique post'
+      )
+      Comment.create!(
+        post: post,
+        body: 'This is a unique post comment'
+      )
+end
+
+
 # Create Posts
-50.times do
+10.times do
     Post.create!(
       title:  Faker::Lorem.sentence,
       body:   Faker::Lorem.paragraph
@@ -8,12 +23,13 @@ require 'faker'
 end
   posts = Post.all
 # Create Comments
-100.times do
+20.times do
     Comment.create!(
       post: posts.sample,
       body: Faker::Lorem.paragraph
   )
 end
+
 puts "Seed finished"  
 puts "#{Post.count} posts created"  
 puts "#{Comment.count} comments created"
